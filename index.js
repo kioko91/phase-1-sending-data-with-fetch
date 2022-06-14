c0a06c055c72fa7e225e4002c07e72ebf42802cb
Calvin <calvinkioko91@gmail.com>
2022-06-14 22:26:10 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+    })
+        .then(resp => resp.json())
+        .then(obj => {
+            console.log(obj);
+            const p = document.createElement("p");
+            p.textContent = obj.id;
+            document.body.appendChild(p)
+        })
+        .catch(err => {
+            const p = document.createElement("p");
+            p.textContent = err.message;
+            document.body.appendChild(p)
+        })
+}
